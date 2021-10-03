fba462eabf5ecebe7e5dd7ecb5eab33365f9de22
Xrsirx5 <91307602+Xrsirx5@users.noreply.github.com>
2021-10-03 15:25:41 +0530

    Create translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,12 +5,10 @@
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
-
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+<i>Am Just A Advance Auto Filter Bot for my Group
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@noughtyland
 </i>"""    
     
     HELP_TEXT = """
@@ -43,12 +41,12 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">Alif muhammed</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
